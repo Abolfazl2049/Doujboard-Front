eff930917cb56970ab9ef48a796992be3ac3f00d
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2026-02-18 21:51:18 +0330

    icons

--- app/core/@services/@shared/constants/index.ts
+++ app/core/@services/@shared/constants/index.ts
@@ -1,5 +1,5 @@
-const BASE_API_URL = "https://api.Doujboard.uk";
-const DOMAIN_URL = "https://dashboard.Doujboard.uk";
+const BASE_API_URL = "https://api.doujboard.uk";
+const DOMAIN_URL = "https://dashboard.doujboard.uk";
 const MAX_PAGE_SIZE = 250;
 const DEFAULT_PAGE_SIZE = 30;
 export { BASE_API_URL, DOMAIN_URL, MAX_PAGE_SIZE, DEFAULT_PAGE_SIZE };
